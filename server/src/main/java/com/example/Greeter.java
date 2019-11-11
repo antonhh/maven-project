d1839f2d9bc4c2817d99e9a9afc7c6d5c9fc29d1
Anton <antonhh@live.dk>
2019-11-11 14:38:34 +0100

    commit final

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,12 @@ public class Greeter {
 
   }
 
+
+  /**
+   *
+   * @param someone
+   * @return
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
